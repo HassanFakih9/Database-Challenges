897798512ade9195c011b9173a5a151c859803f2
Hassan Fakih <hassan.fakih.80@gmail.com>
2023-09-14 10:24:00 +0300

    done with challenge 1 mongoDB

---
+++ mongodb/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+const port = 5000;
+
+const cors = require("cors");
+
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+
+
+    res.send("This is working!");
+});
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+
+});
